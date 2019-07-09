42ef1a2edf05cd4f9a95fa24786f5b0cd8efea67
guuguo <guuguo@qq.com>
2019-07-09 20:25:59 +0800

    ''

---
+++ src/com/company/3_LengthOfLongestSubstring.kt
@@ -0,0 +1,47 @@
+package com.company
+
+import kotlin.test.assertEquals
+
+class LengthOfLongestSubstring {
+    /**
+     * 递归计算next
+     * */
+    fun lengthOfLongestSubstring(s: String): Int {
+        var iStart = 0
+        var maxLength = 0
+        var cLength = 0
+        for ((i, v) in s.withIndex()) {
+            var isBreak = false
+            for (j in iStart until i) {
+                if (s[j] == v) {
+                    iStart = j+1
+                    if (cLength > maxLength)
+                        maxLength = cLength
+                    cLength = i - j
+                    isBreak = true
+                    break
+                }
+            }
+            if(!isBreak){
+                cLength++
+            }
+        }
+        if (cLength > maxLength)
+            maxLength = cLength
+        return maxLength
+    }
+}
+
+fun main(args: Array<String>) {
+    {
+        val obj = LengthOfLongestSubstring()
+        assertEquals(3, obj.lengthOfLongestSubstring("dvdf"))
+        assertEquals(7, obj.lengthOfLongestSubstring("abcdefg"))
+        assertEquals(0, obj.lengthOfLongestSubstring(""))
+        assertEquals(2, obj.lengthOfLongestSubstring("au"))
+        assertEquals(1, obj.lengthOfLongestSubstring(" "))
+        assertEquals(3, obj.lengthOfLongestSubstring("abcabcbb"))
+        assertEquals(1, obj.lengthOfLongestSubstring("bbbbb"))
+        assertEquals(3, obj.lengthOfLongestSubstring("pwwkew"))
+    }.checkTime()
+}
