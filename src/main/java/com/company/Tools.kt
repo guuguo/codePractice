a4bc69780d367b57836717a1f09a31aa9941380b
guuguo <guuguo@qq.com>
2020-09-23 13:43:31 +0800

    feat(leetcode): 新增三数之和

--- src/main/java/com/company/Tools.kt
+++ src/main/java/com/company/Tools.kt
@@ -2,13 +2,22 @@ package com.company
 
 fun (() -> Unit).checkTime() {
     Runnable {
-        val startTime=System.currentTimeMillis()
+        val startTime = System.currentTimeMillis()
         println("--------开始时间:$startTime")
         this()
-        val endTime=System.currentTimeMillis()
+        val endTime = System.currentTimeMillis()
         println("--------结束时间:$endTime")
-        println("--------耗时:${endTime-startTime}毫秒--------")
-
+        println("--------耗时:${endTime - startTime}毫秒--------")
     }.run()
+}
 
+fun <T> T.checkTime(block: () -> Unit) {
+    Runnable {
+        val startTime = System.currentTimeMillis()
+        println("--------开始时间:$startTime")
+        block()
+        val endTime = System.currentTimeMillis()
+        println("--------结束时间:$endTime")
+        println("--------耗时:${endTime - startTime}毫秒--------")
+    }.run()
 }
