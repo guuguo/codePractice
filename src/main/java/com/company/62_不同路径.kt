c6e96df634f26ab2808c42019b44325b44a5c269
liuyun <liuyun@byai.com>
2021-12-08 10:22:28 +0800

    feat:新增61-62

--- src/main/java/com/company/62_不同路径.kt
+++ src/main/java/com/company/62_不同路径.kt
@@ -7,6 +7,17 @@ import java.util.*
  * @author guuguo
  * @see <a href="https://leetcode-cn.com/problems/unique-paths/">leetcode链接</a>*/
 fun uniquePaths(m: Int, n: Int): Int {
-    if(m==1 || n==1) return 1
-    return uniquePaths(m-1,n) +uniquePaths(m,n-1)
+    if(!::array.isInitialized || array.size<m || array[0].size<n){
+        array= Array(m){IntArray(n){0}}
+        array[0][0]=1
+    }
+    return uniquePaths2(m,n)
 }
+fun uniquePaths2(m: Int, n: Int): Int {
+    if (m == 1 || n == 1) return 1
+    if (array[m - 1][n - 1] != 0) return array[m - 1][n - 1]
+    array[m - 1][n - 1] = uniquePaths2(m - 1, n) + uniquePaths2(m, n - 1)
+    return  array[m - 1][n - 1]
+}
+
+lateinit var array:Array<IntArray>
--- src/test/java/Test61-80.kt
+++ src/test/java/Test61-80.kt
@@ -22,12 +22,11 @@ class `Test61-80` {
     fun test62() {
         {
             repeat(10000) {
-                assertEquals(ListNode(2)+3+4+5+1, uniquePaths(2))
-                assertEquals(ListNode(3)+4+5+1+2, rotateRight(ListNode(1)+2+3+4+5,3))
-                assertEquals(ListNode(3)+4+5+1+2, rotateRight(ListNode(1)+2+3+4+5,8))
-                assertEquals(ListNode(3), rotateRight(ListNode(3),100))
-                assertEquals(ListNode(3)+4+5+1+2, rotateRight(ListNode(3)+4+5+1+2,0))
-                assertEquals(null, rotateRight(null,93))
+                assertEquals(1, uniquePaths(2,1))
+                assertEquals(28, uniquePaths(3,7))
+                assertEquals(3, uniquePaths(3,2))
+                assertEquals(6, uniquePaths(3,3))
+                assertEquals(1916797311, uniquePaths(51,9))
             }
         }.checkTime()
     }
